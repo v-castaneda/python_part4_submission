4f6fc90c79808d04b2406891a3fcc7146b689111
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-11-21 20:47:16 -0500

    first commit

---
+++ hwk_functions.py
@@ -0,0 +1,9 @@
+Write a Python function called max_num()to find the maximum of three numbers.
+Write a Python function called mult_list() to multiply all the numbers in a list.
+Write a Python function called rev_string() to reverse a string.
+Write a Python function called num_within() to check whether a number falls in a given range.
+The function accepts the number, beginning of range, and end of range (inclusive) as arguments.
+Examples: num_within(3,2,4) returns True, num_within(3,1,3) returns True, num_within(10,2,5) returns False.
+Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+The function accepts the number n, the number of rows to print
+Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. Each number is the two numbers above it added together.
