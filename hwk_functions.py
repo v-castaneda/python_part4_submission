1dece0eb241b27796ead5004bfad1829dae252b6
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-11-27 23:42:04 -0500

    Completed problem 4

--- hwk_functions.py
+++ hwk_functions.py
@@ -29,15 +29,23 @@ print('Problem 3')
 print(rev_string('Victor'))
 print(rev_string('Hello World'))
 
-# Write a Python function called num_within() to
-# check whether a number falls in a given range.
+# Write a Python function called num_within() to check whether 
+# a number falls in a given range. The function accepts the number, 
+# beginning of range, and end of range (inclusive) as arguments.
+# Examples: 
+# num_within(3,2,4) returns True, 
+# num_within(3,1,3) returns True, 
+# num_within(10,2,5) returns False.
+def num_within(num, range_low, range_high):
+    if num >= range_low and num <= range_high:
+        return True
+    else:
+        return False
 
-# The function accepts the number, beginning of range, 
-# and end of range (inclusive) as arguments.
-
-# Examples: num_within(3,2,4) returns 
-# True, num_within(3,1,3) returns 
-# True, num_within(10,2,5) returns False.
+print('Problem 4')
+print(num_within(3, 2, 4))
+print(num_within(3, 1, 3))
+print(num_within(10, 2, 5))
 
 # Write a Python function called pascal() that 
 # prints out the first n rows of Pascal's triangle.
