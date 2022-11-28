c4424ca43e9742243c7052777b8860b5f3acf243
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-11-28 00:27:12 -0500

    completed problem 5

--- hwk_functions.py
+++ hwk_functions.py
@@ -1,4 +1,4 @@
-# Write a Python function called max_num()to 
+# Write a Python function called max_num()to
 # find the maximum of three numbers.
 def max_num(a, b, c):
     return  max([a, b, c])
@@ -8,7 +8,7 @@ print(max_num(2, 0, 8))
 print(max_num(-5, -3, -18))
 print(max_num(2, 0, -100))
 
-# Write a Python function called mult_list() 
+# Write a Python function called mult_list()
 # to multiply all the numbers in a list.
 def mult_list(num_list):
     product = 1
@@ -29,12 +29,12 @@ print('Problem 3')
 print(rev_string('Victor'))
 print(rev_string('Hello World'))
 
-# Write a Python function called num_within() to check whether 
-# a number falls in a given range. The function accepts the number, 
+# Write a Python function called num_within() to check whether
+# a number falls in a given range. The function accepts the number,
 # beginning of range, and end of range (inclusive) as arguments.
-# Examples: 
-# num_within(3,2,4) returns True, 
-# num_within(3,1,3) returns True, 
+# Examples:
+# num_within(3,2,4) returns True,
+# num_within(3,1,3) returns True,
 # num_within(10,2,5) returns False.
 def num_within(num, range_low, range_high):
     if num >= range_low and num <= range_high:
@@ -47,8 +47,39 @@ print(num_within(3, 2, 4))
 print(num_within(3, 1, 3))
 print(num_within(10, 2, 5))
 
-# Write a Python function called pascal() that 
+# Write a Python function called pascal() that
 # prints out the first n rows of Pascal's triangle.
-
 # The function accepts the number n, the number of rows to print
-# Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. Each number is the two numbers above it added together.
+# Note : Pascal's triangle is an arithmetic and geometric figure first
+# imagined by Blaise Pascal. Each number is the two numbers above it added together.
+
+# initiate triangle
+triangle = [[1],[1,1]]
+def pascal(n):
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      length = len(row_prev)+1
+      for i in range(length):
+        if i == 0:
+          row.append(1)
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+
+    for row in triangle:
+      print(row)
+
+print('Problem 5')
+pascal(0)
+pascal(2)
+pascal(10)
